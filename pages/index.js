2d24c4d8782e060ce06c2d1bcd9b72eed2e2d827
Ali Taee <ali.taee.72@gmail.com>
2019-08-01 19:32:16 +0430

    Adding styled JSX

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,19 @@ const Index = () => (
         <Link href="/about">
             <a title="About Page">About</a>
         </Link>
-        <p>Hello Next.js</p>
+        <p>Hello World</p>
+        <style jsx>{`
+            a {
+                color: #0000ff;
+            }
+        `}</style>
+
+        <style jsx global>{`
+            body {
+                background: #212121;
+                color: #fefefe;
+            }
+        `}</style>
     </div>
 );
 
