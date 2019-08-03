56323ce852b8da34549fa0dc287f621b1eb51798
Ali Taee <ali.taee.72@gmail.com>
2019-08-03 14:54:33 +0430

    Replace node sass width style-jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,22 +1,13 @@
 import React from 'react';
 import { useCounterContext } from '../context/store';
 import HelloWorld from '../components/HelloWorld';
+import '../styles/style.scss';
 
 function Index() {
     return (
         <useCounterContext.Provider>
             <div>
                 <HelloWorld />
-                <style jsx global>
-                    {`
-                    body {
-                        background: #212121;
-                        color: #fefefe;
-                        padding: 0;
-                        margin: 0;
-                    }
-                `}
-                </style>
             </div>
         </useCounterContext.Provider>
     );
