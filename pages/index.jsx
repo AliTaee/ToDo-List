a6f3c020f4ff0b222dc4c69a734ae9c95f19f061
Ali Taee <ali.taee.72@gmail.com>
2019-08-07 09:18:16 +0430

    Add PWA to project

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Head from 'next/head';
 import { useCounterContext } from '../context/store';
 import HelloWorld from '../components/HelloWorld';
 import '../styles/style.scss';
@@ -6,6 +7,15 @@ import '../styles/style.scss';
 function Index() {
     return (
         <useCounterContext.Provider>
+            <Head>
+                <title>Todo list</title>
+                <meta
+                    name="viewport"
+                    content="initial-scale=1.0, width=device-width"
+                    key="viewport"
+                />
+                <script type="text/javascript" src="/static/pwa_script.js" />
+            </Head>
             <div>
                 <HelloWorld />
             </div>
