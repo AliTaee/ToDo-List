ce3ab6ccab4458a8db68e230520e4c8790970d01
Ali Taee <ali.taee.72@gmail.com>
2019-08-08 10:50:50 +0430

    Add create note form UI and favicon

--- components/layout/MainLayout.jsx
+++ components/layout/MainLayout.jsx
@@ -21,6 +21,7 @@ const MainLayout = props => (
         content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
         key="viewport"
       />
+      <link rel="icon" href="/static/favicon.png" />
       <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
       <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
       <script type="text/javascript" src="/static/pwa_script.js" />
--- pages/Create.jsx
+++ pages/Create.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import TextareaAutosize from '@material-ui/core/TextareaAutosize';
+import Button from '@material-ui/core/Button';
 
 // Components
 import Layout from '../components/layout/MainLayout';
@@ -6,6 +9,28 @@ import Layout from '../components/layout/MainLayout';
 const CreateNotes = () => (
     <Layout>
         <h2>Add Note</h2>
+
+        <form id="create-note" noValidate autoComplete="off">
+            <TextField
+                required
+                fullWidth
+                id="title-task"
+                label="Note title"
+                type="text"
+                name="text"
+                margin="normal"
+                variant="outlined"
+            />
+            <TextareaAutosize
+                rows={6}
+                id="content-task"
+                aria-label="note content"
+                placeholder="Note Content"
+            />
+            <Button variant="contained" color="primary">
+                Create Note
+            </Button>
+        </form>
     </Layout>
 );
 
