8917c53544404128199b1e18137878c32429fd27
Ali Taee <ali.taee.72@gmail.com>
2019-08-09 17:59:01 +0430

    Add store, root reducer and simple task reducer for testing

---
+++ redux/reducers/tasksReducer.js
@@ -0,0 +1,8 @@
+const tasks = [
+    { task: 'Learning Vue', content: 'You can do it' },
+    { task: 'Learning React', content: 'Lets Rock' },
+];
+
+export default function tasksReducer(state = tasks, action) {
+    return state;
+}
