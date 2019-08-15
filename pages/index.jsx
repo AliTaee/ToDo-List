23a35923152cd9cc023b3c5bfb87d2486690c55d
AliTaee <ali.taee.72@gmail.com>
2019-08-15 21:49:52 +0430

    Refactor index page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
 
-// React and redux
-import { connect } from 'react-redux';
-
 // Styles
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
@@ -16,28 +13,21 @@ import View from '../components/note/View';
 // Styles
 import '../styles/style.scss';
 
-class Index extends React.Component {
-  render() {
-    return (
-      <main>
-        <Head />
-        <Container>
-          <Grid container spacing={0}>
-            <Grid item xs={12} md={3}>
-              <Menu />
-              <View />
-            </Grid>
-            <Grid className="main" item xs={12} md={9}>
-              <CreateNote />
-            </Grid>
-          </Grid>
-        </Container>
-      </main>
-    );
-  }
-}
+const Index = () => (
+  <main>
+    <Head />
+    <Container>
+      <Grid container spacing={0}>
+        <Grid item xs={12} md={3}>
+          <Menu />
+          <View />
+        </Grid>
+        <Grid className="main" item xs={12} md={9}>
+          <CreateNote />
+        </Grid>
+      </Grid>
+    </Container>
+  </main>
+);
 
-export default connect(
-  null,
-  null,
-)(Index);
+export default Index;
