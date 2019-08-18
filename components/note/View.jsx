2f4b7ec3ea48ac2e685e5a9633a6cb6c22d5d825
AliTaee <ali.taee.72@gmail.com>
2019-08-18 09:21:07 +0430

    Replace unorder list with materail list item in View component

--- components/note/View.jsx
+++ components/note/View.jsx
@@ -2,26 +2,67 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
+// Materail UI
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import Checkbox from '@material-ui/core/Checkbox';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
+
 const shortid = require('shortid');
 
 const View = props => {
   const { tasks } = props;
+
+  const handleToggle = value => {
+    console.log(value);
+  };
+
   return (
     <section>
       <h2>Note List</h2>
-      <ul>
-        {tasks.length === 0 ? (
-          <p>No notes yet.</p>
-        ) : (
-          tasks.map(item => (
-            <li key={shortid.generate()}>
-              {item.task}
-              <br />
-              {item.content}
-            </li>
-          ))
-        )}
-      </ul>
+      {tasks.length === 0 ? (
+        <p>No notes yet.</p>
+      ) : (
+        <List className="note-list">
+          {tasks.map(item => (
+            <ListItem
+              key={shortid.generate()}
+              role={undefined}
+              dense
+              button
+              onClick={handleToggle(shortid.generate())}
+            >
+              <ListItemIcon>
+                <Checkbox
+                  edge="start"
+                  checked={false}
+                  tabIndex={-1}
+                  disableRipple
+                  inputProps={{ 'aria-labelledby': shortid.generate() }}
+                />
+              </ListItemIcon>
+              <ListItemText
+                id={shortid.generate()}
+                primary={`${item.task} ${item.content}`}
+                secondary={`Time`}
+              />
+              <ListItemSecondaryAction>
+                <IconButton edge="end" aria-label="delete">
+                  <DeleteIcon />
+                </IconButton>
+                <IconButton edge="end" aria-label="edit">
+                  <EditIcon />
+                </IconButton>
+              </ListItemSecondaryAction>
+            </ListItem>
+          ))}
+        </List>
+      )}
     </section>
   );
 };
