e92b8153f3bf77e889b846e46371dfa5472bbbe1
AliTaee <ali.taee.72@gmail.com>
2019-08-18 13:52:33 +0430

    Split View component to smaller components, create component for render list item

--- components/note/View.jsx
+++ components/note/View.jsx
@@ -2,36 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 // Redux
-import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import { deleteTask, deleteTasks } from '../../redux/Actions/actionCreator';
 
-// Materail UI
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
-import ListItemText from '@material-ui/core/ListItemText';
-import Checkbox from '@material-ui/core/Checkbox';
-import IconButton from '@material-ui/core/IconButton';
-import Button from '@material-ui/core/Button';
-import DeleteIcon from '@material-ui/icons/Delete';
-import EditIcon from '@material-ui/icons/Edit';
+// Component
+import RenderNoteList from './childComponents/RenderNoteList';
 
 const View = props => {
   const { tasks } = props;
-
-  const handleToggle = value => {
-    console.log(value);
-  };
-
-  const handleDeleteNote = id => {
-    props.deleteTask(id);
-  };
-
-  const handleDeleteAllNote = () => {
-    props.deleteTasks();
-  };
 
   return (
     <section>
@@ -41,41 +18,7 @@ const View = props => {
         <p>No notes yet.</p>
       ) : (
         <div className="list-tasks">
-          <List className="note-list">
-            {tasks.map(item => (
-              <ListItem key={item.id} role={undefined} dense button onClick={handleToggle(item.id)}>
-                <ListItemIcon>
-                  <Checkbox
-                    edge="start"
-                    checked={false}
-                    tabIndex={-1}
-                    disableRipple
-                    inputProps={{ 'aria-labelledby': item.id }}
-                  />
-                </ListItemIcon>
-                <ListItemText
-                  id={item.id}
-                  primary={`${item.task} ${item.content}`}
-                  secondary={`${item.date}`}
-                />
-                <ListItemSecondaryAction>
-                  <IconButton
-                    onClick={() => handleDeleteNote(item.id)}
-                    edge="end"
-                    aria-label="delete"
-                  >
-                    <DeleteIcon />
-                  </IconButton>
-                  <IconButton edge="end" aria-label="edit">
-                    <EditIcon />
-                  </IconButton>
-                </ListItemSecondaryAction>
-              </ListItem>
-            ))}
-          </List>
-          <Button onClick={handleDeleteAllNote} variant="contained" color="secondary">
-            <DeleteIcon /> Delete Notes
-          </Button>
+          <RenderNoteList tasks={tasks} />
         </div>
       )}
     </section>
@@ -84,8 +27,6 @@ const View = props => {
 
 View.propTypes = {
   tasks: PropTypes.array.isRequired,
-  deleteTask: PropTypes.func.isRequired,
-  deleteTasks: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => {
@@ -94,14 +35,7 @@ const mapStateToProps = state => {
   };
 };
 
-const mapDispatchToProps = dispatch => {
-  return {
-    deleteTask: bindActionCreators(deleteTask, dispatch),
-    deleteTasks: bindActionCreators(deleteTasks, dispatch),
-  };
-};
-
 export default connect(
   mapStateToProps,
-  mapDispatchToProps,
+  null,
 )(View);
