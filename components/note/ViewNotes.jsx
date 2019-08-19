eb8ac8dd3da670264151032a65595ce1a57fb0ae
AliTaee <ali.taee.72@gmail.com>
2019-08-19 13:48:52 +0430

    Add button for return to create note when active main is view single note

--- components/note/ViewNotes.jsx
+++ components/note/ViewNotes.jsx
@@ -3,16 +3,30 @@ import PropTypes from 'prop-types';
 
 // Redux
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { activeMain } from '../../redux/Actions/actionMain';
 
 // Components
 import NoteList from './ViewSubComponents/NoteList';
 
+// Materail UI
+import Button from '@material-ui/core/Button';
+
 const View = props => {
-  const { tasks } = props;
+  const { tasks, active } = props;
+
+  const activeAddNote = () => {
+    props.activeMainDispatch('create');
+  };
 
   return (
-    <section>
+    <section className="note-list-container">
       <h3>Note List</h3>
+      {active.name === 'singleNote' && (
+        <Button onClick={activeAddNote} className="add-note" variant="contained" color="primary">
+          Add Note
+        </Button>
+      )}
       {tasks.length === 0 ? <p>No notes yet.</p> : <NoteList tasks={tasks} />}
     </section>
   );
@@ -20,15 +34,24 @@ const View = props => {
 
 View.propTypes = {
   tasks: PropTypes.array.isRequired,
+  activeMainDispatch: PropTypes.func.isRequired,
+  active: PropTypes.object.isRequired,
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    activeMainDispatch: bindActionCreators(activeMain, dispatch),
+  };
 };
 
 const mapStateToProps = state => {
   return {
     tasks: state.tasksReducer,
+    active: state.mainReducer,
   };
 };
 
 export default connect(
   mapStateToProps,
-  null,
+  mapDispatchToProps,
 )(View);
