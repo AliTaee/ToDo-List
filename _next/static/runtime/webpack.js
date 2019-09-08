ad17dce5f14bb158830430e7596f7922d1759832
AliTaee <ali.taee.72@gmail.com>
2019-09-08 10:18:09 +0430

    Deploy Next.js to gh-pages

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c2444fb4a2b16aff284d";
+/******/ 	var hotCurrentHash = "24311dd76d9fc8895e0f";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
