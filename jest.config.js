2ccf51c2d03461a77c8c07b3c489fc361546e4ef
AliTaee <ali.taee.72@gmail.com>
2020-03-19 17:20:20 +0330

    Install some package for testing

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   setupFiles: ['<rootDir>/__test__/setup/setupTests.js'],
-  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  testPathIgnorePatterns: [
+    '<rootDir>/.next/',
+    '<rootDir>/node_modules/',
+    '<rootDir>/__test__/setup/',
+  ],
   moduleNameMapper: {
     '\\.(css|less|scss|sss|styl)$': '<rootDir>/node_modules/jest-css-modules',
   },
