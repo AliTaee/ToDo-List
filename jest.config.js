2091080d68512169cc127353b735ce10fb01831b
AliTaee <ali.taee.72@gmail.com>
2020-03-19 15:00:31 +0330

    Improve Unit test folder

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  setupFiles: ['<rootDir>/setupTests.js'],
+  setupFiles: ['<rootDir>/__test__/setup/setupTests.js'],
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   moduleNameMapper: {
     '\\.(css|less|scss|sss|styl)$': '<rootDir>/node_modules/jest-css-modules',
