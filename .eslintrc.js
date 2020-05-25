3c97cea1a828bf3f882ffbc40ea839a3ce3fffc4
alitaee <ali.taee.72@gmail.com>
2020-05-25 18:56:19 +0430

    Install eslint packages for testing-library

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,18 +9,24 @@ module.exports = {
     strict: 0,
     'linebreak-style': 0,
     'prettier/prettier': 'error',
+    'testing-library/await-async-query': 'error',
+    'testing-library/no-await-sync-query': 'error',
+    'testing-library/no-debug': 'warn',
   },
   settings: {
     react: {
       version: '16.8.6',
     },
   },
-  plugins: ['prettier'],
+  plugins: ['prettier', 'testing-library', 'jest-dom'],
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'prettier',
     'prettier/react',
     'plugin:prettier/recommended',
+    'plugin:testing-library/react',
+    'plugin:testing-library/recommended',
+    'plugin:jest-dom/recommended',
   ],
 };
